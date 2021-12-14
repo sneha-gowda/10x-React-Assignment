e50ab18cfab0b3c90dc05f37369f7711d47cc7fe
sneha-gowda <snehagowda9899@gmail.com>
2021-12-14 16:28:09 +0530

    Assigment completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Data from "./Data.jsx"
+console.log(Data);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Card">
+        <img src={Data[0].profileImg} alt="Profile" width="300px" height="300px" />
+        <div className="Data">
+          <h1>{Data[0].name}</h1>
+          <h2>Location: {Data[0].location}</h2>
+          <h3>Bloog Group: {Data[0].bloodGroup}</h3>
+          <h4>Age: {Data[0].age}</h4>
+        </div>
+      </div>
+    </>
   );
 }
 
